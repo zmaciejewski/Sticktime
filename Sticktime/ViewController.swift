473bbbdf2567f56cf13f193faeda5cfb9c539f15
Zachary Maciejewski <zachmaczbm@gmail.com>
2018-06-09 12:02:53 -0400

    Added Top and Bottom CollectionViews with different name arrays

--- Sticktime/ViewController.swift
+++ Sticktime/ViewController.swift
@@ -7,38 +7,61 @@
 //
 
 import UIKit
-//import DragDropiOS
+import DragDropiOS
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-    @IBOutlet weak var collectionView: UICollectionView!
-    let nameArray = ["player1", "player2", "player3", "player4"]
+class ViewController: UIViewController {
+    @IBOutlet weak var collectionViewTop: UICollectionView!
+    @IBOutlet weak var collectionViewBottom: UICollectionView!
+    let nameArrayTop = ["player1", "player2", "player3", "player4"]
+    let nameArrayBottom = ["player4", "player5", "player6", "player 7","Caleb"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        collectionView.delegate = self
-        collectionView.dataSource = self
+        collectionViewTop.delegate = self
+        collectionViewTop.dataSource = self
+        collectionViewBottom.delegate = self
+        collectionViewBottom.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+}
 
+extension ViewController: UICollectionViewDataSource {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return nameArray.count
+        if collectionView == self.collectionViewTop {
+            return nameArrayTop.count
+        } else {
+            return nameArrayBottom.count
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath as IndexPath) as! CollectionViewCell
         
-        cell.displayContent(title: nameArray[indexPath.row])
+        if collectionView == self.collectionViewTop {
+            cell.displayContent(title: nameArrayTop[indexPath.row])
+        } else {
+            cell.displayContent(title: nameArrayBottom[indexPath.row])
+        }
         
         return cell
     }
     
+}
+
+extension ViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(nameArray[indexPath.row])
+        if collectionView == self.collectionViewTop {
+            print(nameArrayTop[indexPath.row])
+        } else {
+            print(nameArrayBottom[indexPath.row])
+        }
     }
 }
 
